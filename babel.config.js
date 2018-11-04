8d6007254182557bce67a503575cb8e319abacaa
Lars Willighagen <lars.willighagen@gmail.com>
2018-11-04 21:18:16 +0100

    chore(babel): add babel config

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "presets": [
+    ["@babel/env", {"targets": {
+      "node": "6"
+    }}]
+  ],
+  "env": {
+    "test": {
+      "plugins": ["istanbul"]
+    }
+  },
+  "comments": false
+}
+ 
