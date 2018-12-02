bae45b952373993de0c81c847853b589e8fbcc4b
Lars Willighagen <lars.willighagen@gmail.com>
2018-12-02 16:48:31 +0100

    test(core, plugin-ris): fix data

--- packages/core/test/plugin-common.spec.js
+++ packages/core/test/plugin-common.spec.js
@@ -27,7 +27,7 @@ const simple = {
   page: '5441-5444'
 }
 
-const string = '[\n  {\n    id: "Q23571040",\n    type: "article-journal",\n    title: "Correlation of the Base Strengths of Amines 1",\n    DOI: "10.1021/ja01577a030",\n    author: [\n      {\n\tgiven: "H. K.",\n\tfamily: "Hall"\n      }\n    ],\n    issued: [\n      {\n\tdate-parts: [ "1957", "1", "1" ]\n      }\n    ],\n    container-title: "Journal of the American Chemical Society",\n    volume: "79",\n    issue: "20",\n    page: "5441-5444"\n  }\n]'
+const string = '[\n  {\n    id: "Q23571040",\n    type: "article-journal",\n    title: "Correlation of the Base Strengths of Amines 1",\n    DOI: "10.1021/ja01577a030",\n    author: [\n      {\n\tgiven: "H. K.",\n\tfamily: "Hall"\n      }\n    ],\n    issued: {\n      date-parts: [\n\t[ "1957", "1", "1" ]\n      ]\n    },\n    container-title: "Journal of the American Chemical Society",\n    volume: "79",\n    issue: "20",\n    page: "5441-5444"\n  }\n]'
 
 const yearSuffix = [{id: 'a', author: [{literal: 'foo'}], issued: {'date-parts': [[2018]]}, 'year-suffix': 'a'}]
 const label = [{'id': 'b', 'citation-label': 'foo', 'type': 'book'}]
