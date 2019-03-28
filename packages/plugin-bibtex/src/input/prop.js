042b4e03f475266bd13a110cb9c5949b002cb153
Lars Willighagen <lars.willighagen@gmail.com>
2019-03-28 22:21:42 +0100

    feat(plugin-bibtex): add date ranges
    Fix https://github.com/larsgw/citation.js/issues/177

--- packages/plugin-bibtex/src/input/prop.js
+++ packages/plugin-bibtex/src/input/prop.js
@@ -41,6 +41,12 @@ const months = [
 const parseBibtexDate = function (value) {
   if (/{|}/.test(value)) {
     return { literal: value.replace(/[{}]/g, '') }
+  } else if (/[—–]/.test(value)) {
+    return {
+    'date-parts': value
+      .split(/[—–]/)
+      .map(part => parseDate(part)['date-parts'][0])
+    }
   } else {
     return parseDate(value)
   }
