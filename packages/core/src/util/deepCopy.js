5309d08c43031b37a2714d768f24ceae637c6138
Lars Willighagen <lars.willighagen@gmail.com>
2019-07-02 09:38:36 +0200

    fix(core): do not attempt to clone non-standard objects
    Do not attempt to clone any objects that are not `Object`s or `Array`s, 
    as the proper code to do that is 6x as slow.
    
    close #52

--- packages/core/src/util/deepCopy.js
+++ packages/core/src/util/deepCopy.js
@@ -5,9 +5,45 @@
  * @memberof Cite.util
  *
  * @param {Object} obj - Input object
+ * @param {Set} [seen]
  *
  * @return {Object} Duplicated object
+ * @throw {TypeError}
  */
-const deepCopy = (obj) => JSON.parse(JSON.stringify(obj))
+export function deepCopy (value, seen = new Set()) {
+  if (typeof value !== 'object' || value === null || (value.constructor !== Object && value.constructor !== Array)) {
+    return value
+  }
+
+  if (seen.has(value)) {
+    throw new TypeError('Recursively copying circular structure')
+  }
+
+  seen.add(value)
+  let copy
+
+  if (value.constructor === Array) {
+    copy = value.map(value => deepCopy(value, seen))
+  } else {
+    const object = {}
+    for (let key in value) {
+      object[key] = deepCopy(value[key], seen)
+    }
+    copy = object
+  }
+
+  seen.delete(value)
+  return copy
+}
+
+// // Proper way to clone any object (apart from arrays perhaps).
+// // Unfortunately to costly performance-wise.
+// const object = Object.create(Object.getPrototypeOf(value))
+// const descriptors = Object.getOwnPropertyDescriptors(value)
+// for (let key in descriptors) {
+//   const descriptor = descriptors[key]
+//   if (descriptor.value) { descriptor.value = deepCopy(descriptor.value) }
+//   Object.defineProperty(object, key, descriptor)
+// }
 
 export default deepCopy
