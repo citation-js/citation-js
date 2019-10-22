c711d2449adfba109731ee862f5208c10f88421f
Lars Willighagen <lars.willighagen@gmail.com>
2019-10-22 21:03:51 +0200

    chore(test): make testing script
    This should make it unnecessary to copy-paste the value of the 'npm 
    test' command everytime I need to change anything, no the syntax is:
    
        npm test -- lerna-args -- script-args

---
+++ test/run-tests.js
@@ -0,0 +1,28 @@
+const { spawn } = require('child_process')
+const args = process.argv.slice(2)
+const i = args.includes('--') ? args.indexOf('--') : undefined
+
+const lernaArgs = args.slice(0, i)
+const scriptArgs = args.slice(i + 1)
+
+const test = spawn('lerna', [
+  'run',
+  'test',
+  '--no-bail',
+  '--stream',
+  '--ignore',
+  '*/cli',
+  ...lernaArgs,
+  '--',
+  '--',
+  '-r',
+  '@babel/register',
+  ...scriptArgs
+], {
+  windowsHide: true,
+  stdio: 'inherit'
+})
+
+test.on('close', (code) => {
+  process.exit(code)
+})
