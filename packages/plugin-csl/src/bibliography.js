b025f8798e43c3212bcde07bd6fef679205a2f4d
Lars Willighagen <lars.willighagen@gmail.com>
2021-01-25 13:31:46 +0100

    style(plugin-csl): change some whitespace etc.

--- packages/plugin-csl/src/bibliography.js
+++ packages/plugin-csl/src/bibliography.js
@@ -34,24 +34,21 @@ const getAffix = (source, affix) => typeof affix === 'function' ? affix(source) 
  * @return {String} output
  */
 export default function bibliography (data, options = {}) {
-  const {
-    template = 'apa',
-    lang = 'en-US',
-    format = 'text',
-    nosort = false
-  } = options
+  const { template = 'apa', lang = 'en-US', format = 'text', nosort = false } = options
   const ids = options.entry ? [].concat(options.entry) : data.map(({ id }) => id)
 
   const citeproc = prepareEngine(data, template, lang, format)
   const sortedIds = citeproc.updateItems(ids, nosort)
 
   if (options.append || options.prepend) {
-    const { append, prepend } = options
     const items = data.reduce((items, entry) => { items[entry.id] = entry; return items }, {})
 
     citeproc.sys.wrapBibliographyEntry = function (id) {
       const entry = items[id]
-      return [getAffix(entry, prepend), getAffix(entry, append)]
+      return [
+        getAffix(entry, options.prepend),
+        getAffix(entry, options.append)
+      ]
     }
   } else {
     citeproc.sys.wrapBibliographyEntry = () => ['', '']
