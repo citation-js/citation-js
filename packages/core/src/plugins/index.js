4a3951e5ae21c61c7f5cdaac56d5c67efe266cb0
Lars Willighagen <lars.willighagen@gmail.com>
2021-02-07 20:44:34 +0100

    docs(core): fix two @typedef s for plugins

--- packages/core/src/plugins/index.js
+++ packages/core/src/plugins/index.js
@@ -84,13 +84,11 @@ export const has = (ref) => ref in indices
 export const list = () => Object.keys(indices)
 
 /**
- * @typedef {String} pluginRef
- * @memberof module:@citation-js/core.plugins
+ * @typedef {String} module:@citation-js/core.plugins~pluginRef
  */
 
 /**
- * @typedef {Object} plugins
- * @memberof module:@citation-js/core.plugins
+ * @typedef {Object} module:@citation-js/core.plugins~plugins
  * @property {Object<module:@citation-js/core.plugins.input~format,module:@citation-js/core.plugins.input~parsers>} input
  * @property {Object<module:@citation-js/core.plugins.output~formatterName,module:@citation-js/core.plugins.output~formatter>} output
  * @property {Object<module:@citation-js/core.plugins.dict~dictName,module:@citation-js/core.plugins.dict~dict>} dict
