1ede64bce8cbab5697b10cdd03a1494d38ff253c
Lars Willighagen <lars.willighagen@gmail.com>
2022-05-22 01:23:49 +0200

    feat(plugin-csl): update styles and locales
    BREAKING CHANGE: output of updated styles and locales may differ

---
+++ scripts/locales.csl.js
@@ -0,0 +1,12 @@
+/* global fetch */
+require('isomorphic-fetch')
+
+const LOCALES = ['en-US', 'nl-NL', 'fr-FR', 'de-DE', 'es-ES']
+
+Promise.all(LOCALES.map(locale => fetch(`https://raw.githubusercontent.com/citation-style-language/locales/master/locales-${locale}.xml`)
+  .then(response => response.text())
+  .then(text => text.replace(/\n +/g, ''))
+  .then(text => [locale, text])
+)).then(styles => Object.fromEntries(styles))
+  .then(styles => JSON.stringify(styles, null, 2))
+  .then(console.log)
---
+++ scripts/styles.csl.js
@@ -0,0 +1,16 @@
+/* global fetch */
+require('isomorphic-fetch')
+
+const STYLES = {
+  apa: 'apa',
+  vancouver: 'vancouver',
+  harvard1: 'harvard-cite-them-right'
+}
+
+Promise.all(Object.entries(STYLES).map(([name, style]) => fetch(`https://zotero.org/styles/${style}`)
+  .then(response => response.text())
+  .then(text => text.replace(/\n +/g, ''))
+  .then(text => [name, text])
+)).then(styles => Object.fromEntries(styles))
+  .then(styles => JSON.stringify(styles, null, 2))
+  .then(console.log)
