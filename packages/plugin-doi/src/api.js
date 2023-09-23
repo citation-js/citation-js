ebbe42a756ba1ace9e87946a5dd323f8aab09118
Lars Willighagen <lars.willighagen@gmail.com>
2023-09-23 11:58:44 +0200

    refactor(plugin-doi): simplify HTTP requests

--- packages/plugin-doi/src/api.js
+++ packages/plugin-doi/src/api.js
@@ -15,19 +15,17 @@ const apiOptions = {
 }
 
 /**
- * Fetch DOI API results
- *
  * @access private
- * @method fetchDoiApiAsync
- * @memberof module:@citation-js/plugin-doi.parsers.api
- *
- * @param {String} url - The input url
- *
- * @return {Promise<module:@citation-js/core~CSL>} The fetched JSON
+ * @param {String} response
+ * @return {module:@citation-js/core~CSL}
  */
-async function fetchDoiApiAsync (url) {
-  const result = await util.fetchFileAsync(url, apiOptions)
-  return result === '[]' ? {} : JSON.parse(result)
+function processApiResponse (response) {
+  /* istanbul ignore next */
+  if (response === '[]') {
+    return {}
+  }
+
+  return parseDoiJson(JSON.parse(response))
 }
 
 /**
@@ -41,25 +39,9 @@ async function fetchDoiApiAsync (url) {
  *
  * @return {Promise<Array<module:@citation-js/core~CSL>>} Array of CSL
  */
-async function parseDoiApiAsync (data) {
-  const doiJsonList = await Promise.all([].concat(data).map(fetchDoiApiAsync))
-  return doiJsonList.map(parseDoiJson)
-}
-
-/**
- * Fetch DOI API results
- *
- * @access private
- * @method fetchDoiApi
- * @memberof module:@citation-js/plugin-doi.parsers.api
- *
- * @param {String} url - The input url
- *
- * @return {module:@citation-js/core~CSL} The fetched JSON
- */
-function fetchDoiApi (url) {
-  const result = util.fetchFile(url, apiOptions)
-  return result === '[]' ? {} : JSON.parse(result)
+function parseDoiApiAsync (data) {
+  const response = [].concat(data).map(url => util.fetchFileAsync(url, apiOptions).then(processApiResponse))
+  return Promise.all(response)
 }
 
 /**
@@ -74,7 +56,8 @@ function fetchDoiApi (url) {
  * @return {Array<module:@citation-js/core~CSL>} Array of CSL
  */
 function parseDoiApi (data) {
-  return [].concat(data).map(fetchDoiApi).map(parseDoiJson)
+  const response = [].concat(data).map(url => util.fetchFile(url, apiOptions)).map(processApiResponse)
+  return response
 }
 
 export {
